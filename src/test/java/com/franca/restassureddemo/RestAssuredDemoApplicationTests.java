50a4c35e964e40e5a741e2b0bb71319b1e74de08
Rossywan França <rossyfranca@gmail.com>
2023-08-09 17:09:15 -0300

    first commit

---
+++ src/test/java/com/franca/restassureddemo/RestAssuredDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.franca.restassureddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestAssuredDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
