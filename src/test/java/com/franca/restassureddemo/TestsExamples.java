fe410a9aa21779c50f550c04fc0946b681262b2c
Rossywan França <rossyfranca@gmail.com>
2023-08-10 14:40:22 -0300

    mvn clean test temporario

--- src/test/java/com/franca/restassureddemo/TestsExamples.java
+++ src/test/java/com/franca/restassureddemo/TestsExamples.java
@@ -1,14 +1,8 @@
 package com.franca.restassureddemo;
 
-import com.franca.restassureddemo.utils.TestConfig;
-import com.franca.restassureddemo.utils.YamlReader;
 import io.restassured.response.Response;
 import org.apache.http.HttpStatus;
 import org.junit.Test;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Import;
-import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
-
 import static com.franca.restassureddemo.utils.Http.getRequest;
 import static io.restassured.RestAssured.given;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
@@ -16,18 +10,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 
-@SpringJUnitConfig(TestConfig.class)
-@Import(TestConfig.class)
 public class TestsExamples {
 
-    @Test
-    public void testUrlLocalProperty() {
-        String expectedUrl = "http://localhost:3000";
-        String localUrl = YamlReader.getLocalUrl();
 
-        System.out.println("Local URL: " + localUrl);
-        assertEquals(expectedUrl,localUrl );
-    }
     @Test
     public void testOne() {
 
