c9738d45024cc1cb1c75d2232cb668ca752ce03b
Rossywan França <rossyfranca@gmail.com>
2023-08-10 15:09:33 -0300

    login test ok

--- src/test/java/com/franca/restassureddemo/TestsExamples.java
+++ src/test/java/com/franca/restassureddemo/TestsExamples.java
@@ -3,11 +3,15 @@ package com.franca.restassureddemo;
 import io.restassured.response.Response;
 import org.apache.http.HttpStatus;
 import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import static com.franca.restassureddemo.utils.Http.getRequest;
+import static com.franca.restassureddemo.utils.Http.postRequest;
 import static io.restassured.RestAssured.given;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 
 
 public class TestsExamples {
@@ -19,15 +23,31 @@ public class TestsExamples {
         Response response = given().get("http://localhost:3000/usuarios").thenReturn();
         System.out.println(response.getStatusCode());
         System.out.println(response.getBody().asString());
-        assertEquals(HttpStatus.SC_OK,response.statusCode());
+        assertEquals(HttpStatus.SC_OK, response.statusCode());
         response.then().body(matchesJsonSchemaInClasspath("schemas/usuarios-schema.json"));
 
     }
+
     @Test
-    public void testTwo(){
+    public void testTwo() {
         Response response = getRequest("usuarios");
-        assertEquals(HttpStatus.SC_OK,response.statusCode());
+        assertEquals(HttpStatus.SC_OK, response.statusCode());
         response.then().body(matchesJsonSchemaInClasspath("schemas/usuarios-schema.json"));
+    }
+
+    @Test
+    public void testThree() {
+        Map<String, String> body =
+                new HashMap<String, String>() {
+                    {
+                        put("email", "fulano@qa.com");
+                        put("password", "teste");
+                    }
+                };
+        Response response = postRequest("login",body);
+        assertEquals(HttpStatus.SC_OK, response.statusCode());
+        System.out.println(response.getBody().asString());
+
     }
 
 }
--- src/test/java/com/franca/restassureddemo/utils/Http.java
+++ src/test/java/com/franca/restassureddemo/utils/Http.java
@@ -5,6 +5,9 @@ import io.restassured.response.Response;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static io.restassured.RestAssured.given;
 
 @SpringJUnitConfig(TestConfig.class)
@@ -12,10 +15,25 @@ import static io.restassured.RestAssured.given;
 public class Http {
 
     static String localUrl = YamlReader.getLocalUrl();
-    public static Response getRequest(String path){
+
+    public static Response getRequest(String path) {
 
         Response response = given().baseUri(localUrl).get(path).thenReturn();
         return response;
     }
+
+    public static Response postRequest(String path, Map body) {
+
+        Map<String, String> headers =
+                new HashMap<String, String>() {
+                    {
+                        put("Content-type", "application/json");
+                    }
+                };
+        Response response = given().headers(headers).baseUri(localUrl).body(body).post(path).thenReturn();
+
+        return response;
+    }
+
 
 }
